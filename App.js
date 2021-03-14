235f5513b3ba586e8dacdc1c86e77d9a10e1125c
juan socorro <juansoco3@gmail.com>
2021-03-14 17:45:15 -0400

    aiuda

--- App.js
+++ App.js
@@ -8,12 +8,14 @@ import SignIn from './src/screens/signIn';
 import Dashboard from './src/screens/dashboard';
 import Task from './src/screens/Task';
 import DetailsTask from './src/screens/DetailsTask';
+import SearchBar from './src/screens/SearchBar';
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="SearchBar" component={SearchBar} options={{ title: "SearchBar", headerShown: false }}></Stack.Screen>
         <Stack.Screen name="DetailsTask" component={DetailsTask} options={{ title: "DetailsTask", headerShown: false }}></Stack.Screen>
         <Stack.Screen name="Task" component={Task} options={{ title: "Task", headerShown: false }}></Stack.Screen>
         <Stack.Screen options={{ headerShown: false }} name="Dashboard" component={Dashboard}></Stack.Screen>
